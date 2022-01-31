18acfc7fc5363b280ef49104931868e9fb5a8cfe
sophiesucode <sophzsul@gmail.com>
2022-01-31 13:37:44 -0500

    set environment settings using Spring Profiles

---
+++ src/test/java/com/realestateapp/realestateapp/RealEstateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestateapp.realestateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
