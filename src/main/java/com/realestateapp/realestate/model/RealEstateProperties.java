400653ec1b9aafaa04fded1d84ffe67e3c69d6a8
sophiesucode <sophzsul@gmail.com>
2022-02-01 18:27:31 -0500

    created variables and columns for postgres tables for real estate properties

--- src/main/java/com/realestateapp/realestate/model/RealEstateProperties.java
+++ src/main/java/com/realestateapp/realestate/model/RealEstateProperties.java
@@ -1,2 +1,72 @@
-package com.realestateapp.realestate.model;public class RealEstateProperties {
+package com.realestateapp.realestate.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "real_estate_properties")
+public class RealEstateProperties {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String street;
+
+    @Column
+    private String city;
+
+
+    @Column
+    private Integer zipcode;
+
+    @Column
+    private String state;
+
+    @Column
+    private String neighborhood;
+
+    @Column
+    private String property_type;
+
+    @Column
+    private String description;
+
+    @Column
+    private double property_size;
+
+    @Column
+    private String list_date;
+
+    @Column
+    private Integer num_bedrooms;
+
+    @Column
+    private Integer num_bathrooms;
+
+    @Column
+    private Boolean hasPool;
+
+    @Column
+    private Boolean hasBasement;
+
+    @Column
+    private Boolean hasBackyard;
+
+    @Column
+    private Boolean hasGarage;
+
+    @Column
+    private String property_status;
+
+    //FK will be company id and agent id
+
+   
+
+
+
+
+
+
 }
