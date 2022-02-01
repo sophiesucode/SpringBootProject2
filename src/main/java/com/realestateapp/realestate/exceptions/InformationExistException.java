a3046e73d815b8f8bfca071fdfdeb30b31109d29
sophiesucode <sophzsul@gmail.com>
2022-01-31 23:07:36 -0500

    information not found exception added

--- src/main/java/com/realestateapp/realestate/exceptions/InformationExistException.java
+++ src/main/java/com/realestateapp/realestate/exceptions/InformationExistException.java
@@ -1,13 +1,2 @@
-package com.realestateapp.realestate.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-
-
-@ResponseStatus(HttpStatus.CONFLICT)
-public class InformationExistException extends RuntimeException{
-    public InformationExistException(String message){
-        super(message);
-    }
+package com.realestateapp.realestate.exceptions;public class InformationExistException {
 }
