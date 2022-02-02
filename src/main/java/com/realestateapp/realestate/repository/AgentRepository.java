5f545478095b3f987e0891ab3df6d899f03c6228
sophiesucode <sophzsul@gmail.com>
2022-02-02 18:04:44 -0500

    created agent repository

--- src/main/java/com/realestateapp/realestate/repository/AgentRepository.java
+++ src/main/java/com/realestateapp/realestate/repository/AgentRepository.java
@@ -1,2 +1,10 @@
-package com.realestateapp.realestate.repository;public interface AgentRepository {
+package com.realestateapp.realestate.repository;
+
+import com.realestateapp.realestate.model.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgentRepository extends JpaRepository <Agent , Long> {
+//     Agent findAgentsById(String agentName) ;
 }
