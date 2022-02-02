2f173680231218a7995e6cd99efcf4abf0d4a4b5
sophiesucode <sophzsul@gmail.com>
2022-02-02 00:23:15 -0500

    changed controller name to 'RealEstateController' for more clarity

--- src/main/java/com/realestateapp/realestate/controller/CategoryController.java
+++
@@ -1,9 +0,0 @@
-package com.realestateapp.realestate.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/api")
-public class CategoryController {
-}
---
+++ src/main/java/com/realestateapp/realestate/controller/RealEstateController.java
@@ -0,0 +1,24 @@
+package com.realestateapp.realestate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class RealEstateController {
+
+
+//    private CategoryService categoryService;
+//
+//    @Autowired
+//    public void setCategoryService(CategoryService categoryService) {
+//        this.categoryService = categoryService;
+//    }
+//
+//    @GetMapping("/categories")
+//    public List<Category> getCategories() {
+//        System.out.println("calling getCategories ==>");
+//        return categoryService.getAllCategories();
+//    }
+
+}
