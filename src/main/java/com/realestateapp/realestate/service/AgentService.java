8b9e6bf3db8ed9401b47c4eee79864e55ecf7dde
sophiesucode <sophzsul@gmail.com>
2022-02-03 12:34:16 -0500

    injected property repository and agent repository

--- src/main/java/com/realestateapp/realestate/service/AgentService.java
+++ src/main/java/com/realestateapp/realestate/service/AgentService.java
@@ -1,4 +1,30 @@
 package com.realestateapp.realestate.service;
 
+import com.realestateapp.realestate.repository.AgentRepository;
+import com.realestateapp.realestate.repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AgentService {
+
+    private AgentRepository agentRepository; //inject repository
+
+    @Autowired //autowire repository to constructor
+    public void setAgentRepository(AgentRepository agentRepository){
+        this.agentRepository = agentRepository;
+    }
+
+
+//    public AgentRepository getAgentRepository() {//is this necessary? why ?
+//        return agentRepository;
+//    }
+
+    private PropertyRepository propertyRepository;
+
+    @Autowired
+    public void setPropertyRepository(PropertyRepository propertyRepository){
+        this.propertyRepository = propertyRepository;
+    }
 }
+
