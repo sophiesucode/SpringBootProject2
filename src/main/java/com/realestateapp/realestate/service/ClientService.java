e4ef482095b74a8c11c767be85f4950c886e8eb6
sophiesucode <sophzsul@gmail.com>
2022-02-03 23:10:42 -0500

    completed all code with logic for crud methods and handling exceptions

--- src/main/java/com/realestateapp/realestate/service/ClientService.java
+++ src/main/java/com/realestateapp/realestate/service/ClientService.java
@@ -1,8 +1,14 @@
 package com.realestateapp.realestate.service;
 
 
+import com.realestateapp.realestate.exceptions.InformationNotFoundException;
+import com.realestateapp.realestate.model.Agent;
+import com.realestateapp.realestate.model.Client;
 import com.realestateapp.realestate.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ClientService {
 
@@ -12,5 +18,58 @@ public class ClientService {
     public void setClientRepository(ClientRepository clientRepository){
         this.clientRepository = clientRepository;
     }
-}
+
+    public List<Client> getAllClients(){
+        return clientRepository.findAll();
+    }
+
+    public Client createClient(Client clientObject) {
+
+        Client client = clientRepository.findClientByName(clientObject.getName());
+        return clientRepository.save(clientObject);
+    }
+
+    public Optional<Client> getClient(Long clientId) {
+
+        Optional<Client> client = clientRepository.findById(clientId);
+        if(client.isPresent()){
+            return client;
+        }else{
+            throw new InformationNotFoundException("client with Id " + clientId + " not found");
+        }
+    }
+
+    public Client updateClient(Long clientId, Client clientObject) {
+
+        Optional<Client> client = clientRepository.findById(clientId);
+        if (client.isPresent()) {
+            Client updateClient = clientRepository.findById(clientId).get();
+            updateClient.setName(clientObject.getName());
+            updateClient.setEmail(clientObject.getEmail());
+            updateClient.setPhone(clientObject.getPhone());
+            updateClient.setPhone(clientObject.getPhone());
+            updateClient.setMessage(clientObject.getMessage());
+            return clientRepository.save(updateClient);
+        } else {
+            throw new InformationNotFoundException("client with id " + clientId + " not found");
+        }
+    }
+
+        public Optional<Client> deleteClient(Long clientId){
+
+            Optional<Client> client = clientRepository.findById(clientId);
+            if(client.isPresent()){
+                clientRepository.deleteById(clientId);
+                return client;
+
+            }else {
+                throw new InformationNotFoundException("client with id " + clientId +" not found");
+            }
+        }
+    }
+
+
+
+
+
 
