4e91328550eb44b63a3d2029f458b86a7e361b31
sophiesucode <sophzsul@gmail.com>
2022-02-04 02:02:13 -0500

    created api endpoints in property controller

--- src/main/java/com/realestateapp/realestate/controller/PropertyController.java
+++ src/main/java/com/realestateapp/realestate/controller/PropertyController.java
@@ -1,9 +1,80 @@
 package com.realestateapp.realestate.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.realestateapp.realestate.model.Agent;
+import com.realestateapp.realestate.model.Properties;
+
+import com.realestateapp.realestate.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api")
 public class PropertyController {
+
+ private PropertyService propertyService;
+
+ @Autowired
+ public void setPropertyService(PropertyService propertyService){this.propertyService = propertyService;}
+
+
+   @GetMapping(path = "/hello/")
+    public String getHello() {
+       return "Hello World";
+   }
+
+    @GetMapping(path = "/properties/")
+    public List<Properties> getProperties(){
+     System.out.println("calling getProperties ==>");
+        return propertyService.getAllProperties();
+    }
+
+    @GetMapping("/properties/{propertiesId}")
+    public Optional getProperties(@PathVariable Long propertiesId){
+        System.out.println(("calling getProperties ==>"));
+        return propertyService.getProperties(propertiesId);
+    }
+
+    @PostMapping("/properties/")
+   public Properties createProperty(@RequestBody Properties propertiesObject){
+      System.out.println("calling createProperty ==>");
+       return propertyService.createProperty(propertiesObject);
+   }
+
+    @PutMapping("/properties/{propertiesId}")
+   public Properties updateProperties(@PathVariable(value = "propertiesId") Long propertiesId, @RequestBody Properties propertiesObject) {
+        System.out.println("calling updateProperty ==>");
+       return propertyService.updateProperties(propertiesId, propertiesObject);
+    }
+
+    public PropertyService getPropertyService() {
+        return propertyService;
+    }
+
+
 }
+//    @PutMapping("/properties/{propertiesId}")
+//    public Properties updateProperty(@PathVariable(value = "propertiesId") Long propertiesId, @RequestBody Properties propertiesObject) {
+//        System.out.println("calling updateProperty ==>");
+//        return propertyService.updateProperty(propertiesId, propertiesObject);
+//    }
+
+
+
+
+
+
+/*
+get property by type/find by
+get property by id
+get property by status
+get property by neighborhood/zipcode/address
+
+post: create new property
+update property
+
+
+
+*/
