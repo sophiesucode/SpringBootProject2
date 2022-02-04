129bba3ee278da0c8b0c45d93359d4c18a604dad
sophiesucode <sophzsul@gmail.com>
2022-02-04 02:47:50 -0500

    I deleted a method that was causing an error

--- src/main/java/com/realestateapp/realestate/repository/PropertyRepository.java
+++ src/main/java/com/realestateapp/realestate/repository/PropertyRepository.java
@@ -4,7 +4,7 @@ import com.realestateapp.realestate.model.Properties;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropertyRepository extends JpaRepository<Properties, Long> {
-    Properties findById(int id);
+//    Properties findById(propertiesId);
     Properties findByCity(String city);
 
     Properties findByStreet(String street);
