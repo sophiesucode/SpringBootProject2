56573e5fd20c9acd3fd3cd838c6bc7d7ef155038
sophiesucode <sophzsul@gmail.com>
2022-02-03 19:49:13 -0500

    extend jpa repository, findClientByName method added

--- src/main/java/com/realestateapp/realestate/repository/ClientRepository.java
+++ src/main/java/com/realestateapp/realestate/repository/ClientRepository.java
@@ -1,2 +1,7 @@
-package com.realestateapp.realestate.repository;public interface ClientRepository {
+package com.realestateapp.realestate.repository;
+
+import com.realestateapp.realestate.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
