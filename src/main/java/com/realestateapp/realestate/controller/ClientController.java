503d1a6da61bbfd113dd786768e72d5473013abf
sophiesucode <sophzsul@gmail.com>
2022-02-04 03:05:44 -0500

    added method and api endpoint to get all clients

--- src/main/java/com/realestateapp/realestate/controller/ClientController.java
+++ src/main/java/com/realestateapp/realestate/controller/ClientController.java
@@ -1,4 +1,29 @@
 package com.realestateapp.realestate.controller;
 
+import com.realestateapp.realestate.model.Agent;
+import com.realestateapp.realestate.model.Client;
+import com.realestateapp.realestate.service.AgentService;
+import com.realestateapp.realestate.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/api")
 public class ClientController {
+
+    private ClientService clientService;
+
+    @Autowired
+    public void setClientService(ClientService clientService){this.clientService = clientService;}
+
+   @GetMapping(path = "/clients/")
+    public List<Client> getClients(){
+        System.out.println("calling getClients ==>");
+        return clientService.getAllClients();
+    }
+
 }
