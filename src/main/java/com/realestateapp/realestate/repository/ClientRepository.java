cdd4ccea1ab7dc8a666cb9dac5421c17efd57edb
sophiesucode <sophzsul@gmail.com>
2022-02-03 19:52:49 -0500

    extend jpa repo

--- src/main/java/com/realestateapp/realestate/repository/ClientRepository.java
+++ src/main/java/com/realestateapp/realestate/repository/ClientRepository.java
@@ -4,4 +4,5 @@ import com.realestateapp.realestate.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findClientByName(String name);
 }
