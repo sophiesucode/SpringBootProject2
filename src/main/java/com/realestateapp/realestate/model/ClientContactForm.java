f651812df94971681a50c951c45a1bbc370bf584
sophiesucode <sophzsul@gmail.com>
2022-02-03 19:42:06 -0500

    created variables, getters and setters, made table with columns using @entity and @table, to string method added

--- src/main/java/com/realestateapp/realestate/model/ClientContactForm.java
+++ src/main/java/com/realestateapp/realestate/model/ClientContactForm.java
@@ -1,4 +1,81 @@
 package com.realestateapp.realestate.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="clients")
 public class ClientContactForm {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String message;
+
+    public ClientContactForm(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientContactForm{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
