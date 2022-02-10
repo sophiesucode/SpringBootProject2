2345ee55f44551f96959d9409c797e6ac99920a6
sophiesucode <sophzsul@gmail.com>
2022-02-09 23:01:29 -0500

    created method for finding office by address

--- src/main/java/com/realestateapp/realestate/repository/OfficeRepository.java
+++ src/main/java/com/realestateapp/realestate/repository/OfficeRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OfficeRepository extends JpaRepository< RealEstateOffice, Long> {
     RealEstateOffice findByAddress(String address);
+
+
 }
